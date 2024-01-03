49743a3f4aa16159d80e1a2ac5ddc8905ec68700
sizhang899 <155268373+sizhang899@users.noreply.github.com>
2024-01-02 20:42:19 -0500

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -8,37 +8,8 @@ public class Bishop extends Peice {
     }
 
     @Override
-    public void skroinkPeices(Peice[][] pA) {
-        peiceArr = pA;
-    }
-
-    public void checkShouldRender(int x, int y) {
-        if (!(peiceArr[yPos][xPos].getColor().equals(color))) {
-            shouldRender = false;
-        }
-    }
-
-    public boolean getShouldRender() {
-        return shouldRender;
-    }
-
-    // does some equation stuff to find if the selected square is on the same diagonal as the bishop is (I wrote this over a month ago ok)
-    @Override
     public boolean validMove(int dX, int dY, int cX, int cY, String board[][]) {
-//        System.out.println("Wow, you moved a bishop!");
-//        int deltaX = Math.abs(dX - cX);
-//        int deltaY = Math.abs(dY - cY);
-//        if (deltaX == deltaY) {
-//            super.xPos = dX;
-//            super.yPos = dY;
-//            return true;
-//        } else {
-//            System.out.println("invalid bishop move!");
-//            return false;
-//        }
 
-
-        // Check if the move is along a diagonal path
         int deltaX = Math.abs(cX - dX);
         int deltaY = Math.abs(cY - dY);
 
