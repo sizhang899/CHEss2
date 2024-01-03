b8903025ea6b1de4e87803d7b1f7ff5c43474c37
sizhang899 <155268373+sizhang899@users.noreply.github.com>
2024-01-02 20:44:21 -0500

    Update Peice.java

--- Peice.java
+++ Peice.java
@@ -1,14 +1,13 @@
 
 public abstract class Peice {
-    final int WIDTH = 48;
-    final int HEIGHT = 96;
+
     Peice[][] peiceArr;
     protected String peice;
     protected String color;
     protected String COLOR;
     protected int xPos;
     protected int yPos;
-    protected boolean shouldRender = true;
+
     protected int peiceNumber;
 
     public Peice (String p, String c, int n, int x, int y, Peice[][] pA, String COLOR) {
@@ -48,15 +47,12 @@ public abstract class Peice {
         yPos = y;
     }
 
-    public abstract void skroinkPeices(Peice[][] pA);
 
     public abstract boolean validMove(int dX, int dY, int cX, int cY,String board[][]);
     public boolean validCapture(int dX, int dY, int cX, int cY, String board[][]){
         return false;
     }
 
-    public abstract void checkShouldRender(int dX, int dY);
 
-    public abstract boolean getShouldRender();
 
 }
