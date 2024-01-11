8cfe7a615e5d08068fb4a9aef4a77065cfdfcecc
sizhang899 <155268373+sizhang899@users.noreply.github.com>
2024-01-10 20:20:42 -0500

    Update StartMenus.java

--- StartMenus.java
+++ StartMenus.java
@@ -12,35 +12,37 @@ public class StartMenus extends JFrame implements ActionListener {
     ImageIcon iconImg = new ImageIcon("Chess/assets/AppIcon.png");
 
     public StartMenus() {
-        this.setBounds(0, 0, 813, 837);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        this.setIconImage(this.iconImg.getImage());
-        this.startChessGameButton.setBounds(200, 532, 400, 133);
-        this.startMenuBackground.setText("  Board Master");
+        setBounds(0, 0, 813, 837);
+
+        setIconImage(iconImg.getImage());
+        startChessGameButton.setBounds(200, 532, 400, 133);
+        startMenuBackground.setText("  Board Master");
         startChessGameButton.setText("Chess");
         startTTTGameButton.setText("Tic Tac Toe");
-        this.add(this.startChessGameButton);
-        this.startChessGameButton.addActionListener(this);
-        this.startTTTGameButton.setBounds(200, 266, 400, 133);
+        add(startChessGameButton);
+        startChessGameButton.addActionListener(this);
+        startTTTGameButton.setBounds(200, 266, 400, 133);
 
 
 
-        this.add(this.startTTTGameButton);
-        this.startTTTGameButton.addActionListener(this);
-        this.startMenuBackground.setBounds(0, 0, 800, 800);
+        add(startTTTGameButton);
+        startTTTGameButton.addActionListener(this);
+        startMenuBackground.setBounds(0, 0, 800, 800);
 
 
-        this.add(this.startMenuBackground);
-        this.setVisible(true);
+        add(startMenuBackground);
+        setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == this.startChessGameButton) {
+        if (e.getSource() == startChessGameButton) {
 
             new Chessboard();
         }
 
-        if (e.getSource() == this.startTTTGameButton) {
+        if (e.getSource() == startTTTGameButton) {
             new TicTacToe();
         }
 
