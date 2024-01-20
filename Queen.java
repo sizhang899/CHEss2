25d3303ade9f4a40b76b0eb471f0e601a9a26642
sizhang899 <155268373+sizhang899@users.noreply.github.com>
2024-01-20 10:38:27 -0500

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -56,6 +56,43 @@ public class Queen extends Peice {
         if (deltaX != 0 && deltaY != 0 && deltaX != deltaY) {
             return false; // Invalid move: Queen can move only diagonally or straight
         }
+//determines the direction of the move returns 1, 0, -1
+
+        int xDir = Integer.compare(cX, dX);
+        int yDir = Integer.compare(cY, dY);
+//initialize the next position in the move
+        int x = dX + xDir;
+        int y = dY + yDir;
+//goes thorugh the path to find if there is a piece blocking
+        while (x != cX || y != cY) {
+            String yx = (board[y][x]);
+            if (!yx.isEmpty()) {
+                return false;
+            }
+            //goes to next square in the path
+            x += xDir;
+            y += yDir;
+        }
+
+        return true; // Valid Queen move
+    }
+    public boolean validMoveCheckMate(int dX, int dY, int cX, int cY, String[][] board){//used for check without popup
+        if (!board[cY][cX].isEmpty()){
+            return false;
+        }
+        // Check if the destination is within the board bounds
+        if (cX < 0 || cY < 0 || cX >= board.length || cY >= board[0].length) {
+            return false;
+        }
+
+        // Calculate the movement distance in both x and y directions
+        int deltaX = Math.abs(cX - dX);
+        int deltaY = Math.abs(cY - dY);
+
+        // Check if the move is diagonal or straight
+        if (deltaX != 0 && deltaY != 0 && deltaX != deltaY) {
+            return false; // Invalid move: Queen can move only diagonally or straight
+        }
 
         int xDir = Integer.compare(cX, dX);
         int yDir = Integer.compare(cY, dY);
