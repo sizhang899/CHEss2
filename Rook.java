fa2d0c01c182a65f8d18ecd7a0a08b40836e62da
sizhang899 <155268373+sizhang899@users.noreply.github.com>
2024-01-20 10:38:49 -0500

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -46,6 +46,37 @@ public class Rook extends Peice {
         if (dX != cX && dY != cY) {
             return false;
         }
+//determines the direction of the move
+        int deltaX = Integer.compare(cX, dX);//returns 1, 0 , -1
+        int deltaY = Integer.compare(cY, dY);
+//starts the next position based on the direction
+        int x = dX + deltaX;
+        int y = dY + deltaY;
+//Iteration of the path until the position it wants to go to
+        while (x != cX || y != cY) {
+            String yx = (board[y][x]);
+            if (!yx.isEmpty()) {
+                // There's a piece blocking the rook's path
+
+                return false;
+            }
+            //goes to next position in the path
+            x += deltaX;
+            y += deltaY;
+        }
+        RookHasMoved = true;
+        // Destination is valid, no pieces blocking the path
+        return true;
+    }
+    public boolean validMoveCheckMate(int dX, int dY, int cX, int cY, String[][] board) {
+
+        if (!board[cY][cX].isEmpty()){
+            return false;
+        }
+        // Check if the destination is either along the same row or the same column
+        if (dX != cX && dY != cY) {
+            return false;
+        }
 
         int deltaX = Integer.compare(cX, dX);
         int deltaY = Integer.compare(cY, dY);
