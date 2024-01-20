ffc34077a909c1e64e341c0e13cf942f1471e86d
sizhang899 <155268373+sizhang899@users.noreply.github.com>
2024-01-20 10:36:15 -0500

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -19,7 +19,7 @@ public class Bishop extends Peice {
         // Determine the direction of movement
         int xDir = Integer.compare(cX, dX);
         int yDir = Integer.compare(cY, dY);
-
+//initialize the first position in the move
         int x = dX + xDir;
         int y = dY + yDir;
 
@@ -31,6 +31,7 @@ public class Bishop extends Peice {
 
                 return false; // There's a piece blocking the bishop's path
             }
+            //goes to next square in the path
             x += xDir;
             y += yDir;
         }
@@ -43,6 +44,36 @@ public class Bishop extends Peice {
         return validMove(dX, dY, cX, cY, board);
     }
     public boolean validCaptureCheck(int dX, int dY, int cX, int cY, String[][] board) {
+        int deltaX = Math.abs(cX - dX);
+        int deltaY = Math.abs(cY - dY);
+
+        if (deltaX != deltaY) {
+            return false;
+        }
+
+        // Determine the direction of movement
+        int xDir = Integer.compare(cX, dX);
+        int yDir = Integer.compare(cY, dY);
+
+        int x = dX + xDir;
+        int y = dY + yDir;
+
+        // Check for pieces blocking the bishop's path
+        while (x != cX && y != cY) {
+            String yx = (board[y][x]);
+            if (!yx.isEmpty()) {
+                return false; // There's a piece blocking the bishop's path
+            }
+            x += xDir;
+            y += yDir;
+        }
+
+        return true; // Move is valid - no pieces blocking the path
+    }
+    public boolean validMoveCheckMate(int dX, int dY, int cX, int cY, String[][] board) {
+        if (!board[cY][cX].isEmpty()){
+            return false;
+        }
         int deltaX = Math.abs(cX - dX);
         int deltaY = Math.abs(cY - dY);
 
